34df63cc95fbc118044ebb4eeb14244ce8e6b85f
Håkon Malmedal <hmalmedal@gmail.com>
2015-07-21 08:45:31 +0200

    Allow links

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -1,5 +1,5 @@
 context("file")
 
 test_that("missing file gives error", {
-  expect_error(read_fdb_csv("missing file"), "File does not exist")
+  expect_error(read_fdb_csv("missing file"), "does not exist")
 })
